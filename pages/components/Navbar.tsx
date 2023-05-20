b2806ba3eb41f77fc3bc4bd1ad3a4d3443de3248
Yves Hendseth <yves.hendseth@hispeed.ch>
2023-05-20 14:50:31 +0200

    closing menu on link click

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -4,24 +4,22 @@ import { FaBars, FaEnvelope, FaInstagram, FaTimes } from "react-icons/fa";
 
 import Logo from "./Logo";
 
-
 interface NavbarProps {
-    className?: string;
+  className?: string;
 }
 
 export default function Navbar(props: NavbarProps) {
-  
   const [menuOpen, setMenuOpen] = useState(false);
 
   const handleToggle = () => {
     setMenuOpen(!menuOpen);
-  }
+  };
 
-    return (
+  return (
     <nav className={props.className}>
       <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8 md:h-[100px]">
         <div className="flex items-center justify-between h-full">
-        <div className="hidden md:block md:basis-1/3 h-full">
+          <div className="hidden md:block md:basis-1/3 h-full">
             <div className="flex items-center space-x-4 h-full">
               <Link
                 href="/coffee"
@@ -38,17 +36,21 @@ export default function Navbar(props: NavbarProps) {
             </div>
           </div>
           <div className="flex grow shrink-0 basis-0 items-center justify-start md:justify-center mx-auto h-full">
-            <Link href="/" className="h-full">
+            <Link
+              onClick={() => setMenuOpen(false)}
+              href="/"
+              className="h-full"
+            >
               <Logo height={50} />
             </Link>
           </div>
           <div className="basis-1/3 flex justify-end space-x-4 text-black">
             <FaInstagram className="fill-current h-6 w-6" />
-            <Link href="/contact">
-            <FaEnvelope className="fill-current h-6 w-6" />
+            <Link onClick={() => setMenuOpen(false)} href="/contact">
+              <FaEnvelope className="fill-current h-6 w-6" />
             </Link>
-          
-          <button
+
+            <button
               type="button"
               className="md:hidden text-black focus:outline-none focus:text-black h-6 w-6"
               onClick={handleToggle}
@@ -59,25 +61,36 @@ export default function Navbar(props: NavbarProps) {
                 <FaBars className="fill-current h-6 w-6" />
               )}
             </button>
-            </div>
-
+          </div>
         </div>
       </div>
       {menuOpen && (
-          <div className="md:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
-              <Link href="/" className="text-black block px-3 py-2 rounded-md text-base font-medium">
-                  Home
-              </Link>
-              <Link href="/coffee" className="text-black block px-3 py-2 rounded-md text-base font-medium">
-                  Coffee
-              </Link>
-              <Link href="/story" className="text-black block px-3 py-2 rounded-md text-base font-medium">
-                  Our Story
-              </Link>
-            </div>
+        <div className="md:hidden">
+          <div className="px-2 pt-2 pb-3 space-y-1">
+            <Link
+              onClick={() => setMenuOpen(false)}
+              href="/"
+              className="text-black block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Home
+            </Link>
+            <Link
+              onClick={() => setMenuOpen(false)}
+              href="/coffee"
+              className="text-black block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Coffee
+            </Link>
+            <Link
+              onClick={() => setMenuOpen(false)}
+              href="/story"
+              className="text-black block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Our Story
+            </Link>
           </div>
-        )}
+        </div>
+      )}
     </nav>
   );
 }
