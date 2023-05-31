94128c348befd48740c27393282f4536f7136d9f
Yves Hendseth <yves.hendseth@hispeed.ch>
2023-05-31 17:07:39 +0200

    removed secret

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    postmarkApiKey: "2dea42d2-444e-4c3d-adf0-808a37b23b36",
-    emailFrom: "yves.hendseth@hispeed.ch",
-    emailTo: "yves.hendseth@hispeed.ch",
-  }
 }
 
 module.exports = nextConfig
