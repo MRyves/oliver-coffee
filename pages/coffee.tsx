639a563394af63ebb50aee2ae13cfeaaac077365
Yves Hendseth <yves.hendseth@hispeed.ch>
2023-07-10 14:03:27 +0200

    translating coffee page

--- pages/coffee.tsx
+++ pages/coffee.tsx
@@ -5,14 +5,19 @@ import Link from "next/link";
 import StarterpackPic from "../public/starterpack.jpg";
 import CoffeeVariationPic from "../public/coffee-variations.jpg";
 import CoffeeBanner from "../src/components/Banners/CoffeeBanner";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 export default function CoffeePage() {
+
+const {t} = useTranslation("coffee");
+
   return (
     <>
       <CoffeeBanner />
       <section id="more-info" className="px-7 lg:px-3 max-w-screen-2xl mx-auto mt-3">
-        <Title text="What is in the starterpack?" />
-        <Subtitle text="and how do i get one..." />
+        <Title text={t("whatIs.title")} />
+        <Subtitle text={t("whatIs.subtitle")} />
         <div className="flex flex-col md:flex-row justify-between mt-5">
           <div className="w-full md:block md:w-1/3 h-fit">
             <Image
@@ -25,29 +30,18 @@ export default function CoffeePage() {
           </div>
           <div className="self-center pt-2 md:pt-0 md:w-3/5 md:text-lg">
             <span>
-              Introducing the perfect starter kit for brewing Vietnamese coffee!
-              This all-in-one package includes:
+              {t("whatIs.text")}
               <ul className="list-disc list-outside pl-4 ">
                 <li>
-                  Filter: A high-quality coffee filter, designed specifically
-                  for Vietnamese coffee brewing. It ensures a smooth and robust
-                  cup of coffee every time.
+                  {t("whatIs.list.1")}
+                </li>
+                <li>{t("whatIs.list.2")}
                 </li>
                 <li>
-                  1kg Olivier Coffee: Already grouded coffee beans straight out
-                  of Vietnam
+                {t("whatIs.list.3")}
                 </li>
                 <li>
-                  1 Can Condensed Milk: Indulge in the traditional sweetness of
-                  Vietnamese coffee with a can of rich and creamy condensed
-                  milk. It adds a unique touch to your brew, creating the
-                  perfect balance of flavors
-                </li>
-                <li>
-                  Small Surprise: As an extra treat, we&apos;ve included a small
-                  surprise in your starter kit! Discover a little something
-                  special that will enhance your coffee brewing journey even
-                  further
+                {t("whatIs.list.4")}
                 </li>
               </ul>
             </span>
@@ -56,7 +50,7 @@ export default function CoffeePage() {
                 <button
                   className="p-3 rounded text-white bg-default-brown-normal"
                 >
-                  Get your starterpack!
+                  {t("whatIs.button")}
                 </button>
               </Link>
             </p>
@@ -68,34 +62,24 @@ export default function CoffeePage() {
       </div>
       <section className="px-7 lg:px-3 max-w-screen-2xl mt-3 mx-auto">
         <Title
-          text="What can you do with the starterpack?"
+          text={t("whatDo.title")}
           classNames="md:text-right"
         />
         <Subtitle
-          text="... the traditional recipes"
+          text={t("whatDo.subtitle")}
           classNames="md:text-right"
         />
         <div className="flex flex-col-reverse md:flex-row justify-between mt-5">
           <div className="self-center pt-2 md:pt-0 md:w-3/5 md:text-right md:text-lg">
             <span>
-              Immerse yourself in the world of Vietnamese coffee with our
-              carefully curated starter pack. It&apos;s a gateway to experiencing the
-              unique flavors and brewing traditions of Vietnam, right in your
-              kitchen. This pack gives you the opportunity to prepare your own
-              authentic Vietnamese drip coffee, renowned for its robust and
-              satisfying flavors. Experiment with varying strengths, add
-              sweetened condensed milk for a traditional Vietnamese taste, or
-              enjoy it black to savor the beans&apos; true essence. Whether you&apos;re
-              hosting a coffee tasting party or enjoying a quiet morning, our
-              Vietnamese coffee starter pack will transport you to the vibrant
-              coffee culture of Vietnam.
+              {t("whatDo.text")}
             </span>
             <p className="text-center mt-2">
               <Link href="/recipes">
                 <button
                   className="p-3 rounded text-white bg-default-brown-normal"
                 >
-                  Go to the recipes
+                  {t("whatDo.button")}
                 </button>
               </Link>
             </p>
@@ -116,4 +100,12 @@ export default function CoffeePage() {
       </section>
     </>
   );
+}
+
+export async function getStaticProps({ locale }: { locale: string }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["coffee", "footer", "header"])),
+    },
+  };
 }
--- src/components/Banners/CoffeeBanner.tsx
+++ src/components/Banners/CoffeeBanner.tsx
@@ -1,6 +1,7 @@
 import styles from "../../../styles/Banners/CoffeeBanner.module.css";
 import { FaCoffee } from "react-icons/fa";
 import { MouseEvent } from "react";
+import { useTranslation } from "next-i18next";
 
 export default function CoffeeBanner() {
   const handleClick = (event: MouseEvent) => {
@@ -9,22 +10,24 @@ export default function CoffeeBanner() {
     anchorTag?.scrollIntoView({ behavior: "smooth", block: "start" });
   };
 
+  const {t} = useTranslation("coffee");
+
   return (
     <div id="banner" className={styles.banner}>
       <div className="relative z-10 flex flex-wrap items-center px-4 pt-24 md:px-20 2xl:px-40 md:pt-54 2xl:pt-96">
         <h1 className="flex-1 basis-full text-white text-3xl md:text-6xl font-semibold font-mono">
-          This is our Coffee
+        {t("banner.title")}
         </h1>
         <h3 className="flex-1 basis-full text-white text-xl md:text-3xl font-mono mt-6">
-          Order your starterpack here, <br />
-          straight outta Vietnam!
+          {t("banner.subtitle.1")}<br />
+          {t("banner.subtitle.2")}
         </h3>
         <button
           onClick={handleClick}
           className="inline-flex items-center font-mono text-white rounded-md p-3 mt-3 bg-default-brown-normal md:text-xl"
         >
           <FaCoffee className="fill-current mr-2" />
-          More info!
+          {t("banner.button")}
         </button>
       </div>
     </div>
